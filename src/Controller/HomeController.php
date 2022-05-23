ea4eb064299b7f00b9ebc4eeca53c65fa8b64ffb
Hakweb <massya42@gmail.com>
2022-05-23 18:30:20 +0200

    Part 3 BDD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+
+use App\Entity\Fruit;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+
     #[Route('/', name: 'home_page')]
-    
-    public function index(): Response
-    
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+    public function index(){
+
+        $banane = new Fruit();
+
+        $banane->setName("Jack");
+
+        dump( $banane);
+        return $this->render('home/index.html.twig', [ 'jack' => $banane]);
     }
+
+
 }
+
